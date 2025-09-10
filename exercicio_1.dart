69994d043faeba78997bc4d1872f5456ad9f09b3
menezesstefanny-lab <menezes.stefanny@uni9.edu.br>
2025-09-10 23:36:10 +0000

    exercicio_1.dart

---
+++ exercicio_1.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+void main() {
+    int idade = 15;
+
+if (idade >= 16) {
+    print ("você pode votar este ano!"); }
+    else {
+        print ("você ainda não pode votar.");
+    }
+
+}
