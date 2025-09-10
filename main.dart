1e379e23fda3191ce5fe73105b9431de3bdcf7d2
menezesstefanny-lab <menezes.stefanny@uni9.edu.br>
2025-09-10 23:12:51 +0000

    exercicio_1.dart

--- main.dart
+++ main.dart
@@ -1,4 +1,16 @@
-void main(){
-  print("olá mundo!");
+import 'dart:io'
+
+void main () {
+  bool cadastrarOutroAluno = true;
+  while (cadastrarOutroAluno)
+  print ("informe seu nome:");
+  String? nome = stdin.readLineSync();
+
+  if (nome == "Stefanny"){
+  print ("Olá, $nome");
+  }else{
+    print ("oi, $nome");
+  }
 }
+
 
